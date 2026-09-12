dc677c506d22f49f7efb2a6806a1a75a02db93c9
Camilocastn2007 <brayancamilogt@gmail.com>
2026-09-12 18:29:39 -0500

    Se agrega styles :con variables,flexbox ,ademas se agrega las variables de los discos

---
+++ data/discos.js
@@ -0,0 +1,39 @@
+// data/discos.js
+
+const catalogoDiscos = [
+  // --- URBANO ---
+  { id: "u1", artista: "Quevedo", titulo: "Buenas Noches", genero: "reggaeton", precio: 145000, imagen: "img/discos/Urbano/BUENASNOCHES.jpg" },
+  { id: "u2", artista: "Post Malone / Swae Lee", titulo: "Girasoles", genero: "reggaeton", precio: 155000, imagen: "img/discos/Urbano/GIRASOLES.jpg" },
+  { id: "u3", artista: "Feid", titulo: "Intershibuya", genero: "reggaeton", precio: 160000, imagen: "img/discos/Urbano/INTERSHIBUYA.jpg" },
+  { id: "u4", artista: "Omar Courtz", titulo: "Mi Primera Musa", genero: "reggaeton", precio: 135000, imagen: "img/discos/Urbano/MIPRIMERAMUSA.jpg" },
+  { id: "u5", artista: "Mora", titulo: "Paraíso", genero: "reggaeton", precio: 150000, imagen: "img/discos/Urbano/PARAISO Mora.jpg" },
+  { id: "u6", artista: "Mora", titulo: "Primer Día de Clases", genero: "reggaeton", precio: 140000, imagen: "img/discos/Urbano/PRIMERDIADECLASES.jpg" },
+  { id: "u7", artista: "Dei V", titulo: "Quién es Dei V?", genero: "reggaeton", precio: 130000, imagen: "img/discos/Urbano/QUIENESDEIV.jpg" },
+  { id: "u8", artista: "Álvaro Díaz", titulo: "Sayonara", genero: "reggaeton", precio: 145000, imagen: "img/discos/Urbano/SAYONARA.jpg" },
+  { id: "u9", artista: "Jhayco", titulo: "Timelezz", genero: "reggaeton", precio: 150000, imagen: "img/discos/Urbano/TIMELEZZ.jpg" },
+  { id: "u10", artista: "Rauw Alejandro", titulo: "Vice Versa", genero: "reggaeton", precio: 155000, imagen: "img/discos/Urbano/VICEVERSA.jpg" },
+
+  // --- SALSA ---
+  { id: "s1", artista: "Willie Colón", titulo: "Cosa Nuestra", genero: "salsa", precio: 125000, imagen: "img/discos/Salsa/COSANUESTRA.jpg" },
+  { id: "s2", artista: "Héctor Lavoe", titulo: "La Voz", genero: "salsa", precio: 130000, imagen: "img/discos/Salsa/LAVOZ.jpg" },
+  { id: "s3", artista: "Rubén Blades", titulo: "Siembra", genero: "salsa", precio: 140000, imagen: "img/discos/Salsa/SIEMBRA.jpg" },
+
+  // --- ROCK EN INGLÉS ---
+  { id: "ri1", artista: "Arctic Monkeys", titulo: "AM", genero: "rock-ingles", precio: 165000, imagen: "img/discos/Rock en ingles/AM.jpg" },
+  { id: "ri2", artista: "Guns N' Roses", titulo: "Appetite for Destruction", genero: "rock-ingles", precio: 170000, imagen: "img/discos/Rock en ingles/APETITEFORDESTRUCTION.jpg" },
+  { id: "ri3", artista: "Chase Atlantic", titulo: "Chase Atlantic", genero: "rock-ingles", precio: 145000, imagen: "img/discos/Rock en ingles/CHASEATLANTIC.jpg" },
+  { id: "ri4", artista: "TV Girl", titulo: "French Exit", genero: "rock-ingles", precio: 155000, imagen: "img/discos/Rock en ingles/FRENCHEXIT.jpg" },
+  { id: "ri5", artista: "The Neighbourhood", titulo: "I Love You.", genero: "rock-ingles", precio: 150000, imagen: "img/discos/Rock en ingles/ILOVEYOU.jpeg" },
+  { id: "ri6", artista: "The Strokes", titulo: "Is This It", genero: "rock-ingles", precio: 160000, imagen: "img/discos/Rock en ingles/ISTHISHIT.jpeg" },
+  { id: "ri7", artista: "Nirvana", titulo: "Nevermind", genero: "rock-ingles", precio: 175000, imagen: "img/discos/Rock en ingles/NEVERMIND.jpeg" },
+  { id: "ri8", artista: "The Last Shadow Puppets", titulo: "The Age of the Understatement", genero: "rock-ingles", precio: 165000, imagen: "img/discos/Rock en ingles/THEAGEOFTHEUNDERSTANMENDT.jpg" },
+
+  // --- ROCK EN ESPAÑOL ---
+  { id: "re1", artista: "Gustavo Cerati", titulo: "Bocanada", genero: "rock-pop-espanol", precio: 180000, imagen: "img/discos/Rock en español/BOCANADA.jpg" },
+  { id: "re2", artista: "Soda Stereo", titulo: "Canción Animal", genero: "rock-pop-espanol", precio: 175000, imagen: "img/discos/Rock en español/CANCIONANIMAL.jpg" },
+  { id: "re3", artista: "Los Prisioneros", titulo: "Corazones", genero: "rock-pop-espanol", precio: 140000, imagen: "img/discos/Rock en español/CORAZONES.jpg" },
+  { id: "re4", artista: "Zoé", titulo: "MTV Unplugged", genero: "rock-pop-espanol", precio: 150000, imagen: "img/discos/Rock en español/MTVUNPLUGE.jpg" },
+  { id: "re5", artista: "Luis Miguel", titulo: "Romance", genero: "rock-pop-espanol", precio: 160000, imagen: "img/discos/Rock en español/ROMANCE.jpeg" },
+
+];
+
